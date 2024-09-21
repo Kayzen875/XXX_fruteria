afa7124e98d8efbf7310988d4dc1374e0850e0e3
Kayzen875 <sadrian2612@gmail.com>
2024-09-21 04:26:38 +0200

    Fruteria acabada

--- src/Frutas.jsx
+++ src/Frutas.jsx
@@ -1,49 +1,73 @@
 import { useState } from "react";
 
-function Productos(){
+function Productos() {
 
-    const [total, setTotal] = useState([]); 
+    const [total, setTotal] = useState(0);
     const [ticket, setTicket] = useState([]);
-    const [precio, setPrecio] = useState(0);
 
     const productes = [
         {
-            "id" : 1,
-            "nom" : "Plàtan",
-            "preu" : 0.5
+            "id": 1,
+            "nom": "Plàtan",
+            "preu": 0.5
         },
         {
-            "id" : 2,
-            "nom" : "Poma",
+            "id": 2,
+            "nom": "Poma",
             "preu": 0.8
         },
         {
-            "id" : 3,
-            "nom" : "Pinya",
+            "id": 3,
+            "nom": "Pinya",
             "preu": 5
         },
         {
-            "id" : 4,
-            "nom" : "Meló",
+            "id": 4,
+            "nom": "Meló",
             "preu": 6
         },
     ];
-    
-    return(
-        <div>
-            {productes.map(e => <div><p>{e.nom + " (" + e.preu + " €/u)"}</p> <button id={e.id} onClick={() => Afegir(e)}>Afegir</button></div>)}
-            {ticket.map(e => <div><p>{e}</p></div>)}
-            <div><p>Total: {precio} €</p></div>
+
+    return (
+        <div className="gridContainer">
+            <div>
+                {productes.map(e => <div className="producto" key={e.id}><p>{e.nom + " (" + e.preu + " €/u)"}</p> <span className="button" onClick={() => Afegir(e)}>Afegir</span></div>)}
+            </div>
+            <div>
+                {ticket.map(e => <div className="carrito" key={e.id}><p>{e.nom}</p><span className="button right" onClick={() => Treure(e)}>Treure</span><p>{e.quantity}u x {e.preu} €/u = {(e.quantity * e.preu).toFixed(2)}</p></div>)}
+                <div className="total"><p style={{marginLeft : "5px"}}>Total: {total.toFixed(2)} €</p></div>
+            </div>
         </div>
     )
 
-    function Afegir(e){
-        setTicket([...ticket, e.nom, e.preu]);
-        setTotal(...total, e.preu);
+    function Afegir(e) {
+
+        let updatedTicket;
+
+        if (ticket.find(fruit => fruit.id == e.id)) {
+            updatedTicket = ticket.map(fruit => fruit.id == e.id ? { ...fruit, quantity: fruit.quantity + 1 } : fruit);
+        }
+        else {
+            updatedTicket = [...ticket, { ...e, quantity: 1 }];
+        }
+
+
+        setTicket(updatedTicket);
+        Calcular(updatedTicket);
     }
 
-    function Treure(){
+    function Treure(e) {
 
+        const filteredTicket = ticket.filter(ticket => ticket.id !== e.id);
+
+        setTicket(filteredTicket);
+
+        Calcular(filteredTicket);
+    }
+
+    function Calcular(updatedTicket) {
+
+        setTotal(updatedTicket.reduce((accumulator, fruit) => accumulator + (fruit.preu * fruit.quantity), 0));
     }
 }
 
