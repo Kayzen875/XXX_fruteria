215dde2baaa087a782d3a10a49edcd6cbcfb215a
Kayzen875 <sadrian2612@gmail.com>
2024-09-25 17:00:00 +0200

    Pruebas fetch
    se quita el display de la fruteria

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
 import Productos from "./Frutas"
 
 function App() {
 
+  const [usuarios, setUsuarios] = useState([]);
+
+  function consultaDatos(){
+
+    const opciones = {
+      method: "GET"
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/users", opciones).then(respuesta => respuesta.json()).then(datos => setUsuarios(datos)).then().catch(err => console.log(err));
+  }
+
   return (
     <>
-      <h1>Fruiteria</h1>
+      <h1>Pruebas Fetch</h1>
+      <button onClick={consultaDatos}>Pedir datos</button>
+
+      <ul>
+        {usuarios.map(usuario => <li>{usuario.name}</li>)}
+      </ul>
       
-      <Productos />
+      {/*<Productos />*/}
     </>
   )
 }
